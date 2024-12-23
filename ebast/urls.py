ab203ba465c092ab5a3d53da58340c3c2a3fea55
Fajar Tri Haryanto <fajar.haryanto@bmkg.go.id>
2024-12-23 16:24:18 +0700

    Add homepage view and template; update settings and URLs for new structure

--- ebast/urls.py
+++ ebast/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import HomeView
 
 urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
     path('admin/', admin.site.urls),
     path('qc/', include('qc.urls')),
 ]
