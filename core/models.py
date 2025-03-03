c2a481c8bfca6b47cc71d487e5c5be1b9d760fa7
Fajar Tri Haryanto <fajar.haryanto@bmkg.go.id>
2025-03-03 18:14:43 +0700

    Refactor Kelompok model to change member field to CharField; add operator suggestion feature in form

--- core/models.py
+++ core/models.py
@@ -12,7 +12,7 @@ class Operator(models.Model):
 class Kelompok(models.Model):
     KELOMPOK_CHOICES = models.IntegerChoices('Kelompok', '1 2 3 4 5 6')
     name = models.IntegerField(choices=KELOMPOK_CHOICES.choices, default=1)
-    member = models.ManyToManyField(Operator)
+    member = models.CharField(max_length=300, default='')
 
     def __str__(self):
         return str(self.name)
