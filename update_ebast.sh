cfeb8e95bb29f2583af4e366c4b5ca0845bd20c1
Fajar Tri Haryanto <fajar.haryanto@bmkg.go.id>
2025-04-17 16:23:09 +0700

    Remove top margin from page header in kelompok form styles. Add collectstatic to the update_ebast.sh

--- update_ebast.sh
+++ update_ebast.sh
@@ -1,5 +1,5 @@
 mv db.sqlite3 ..
 git pull
 mv ../db.sqlite3 .
-python manage.py makemigrations && python manage.py migrate
+python manage.py makemigrations && python manage.py migrate && python manage.py collectstatic --noinput
 sudo systemctl restart ebast.service
