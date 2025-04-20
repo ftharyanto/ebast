13831ddf3807c9ea64ae2939b7d2c47a0fdd5ac9
Fajar Tri Haryanto <fajar.haryanto@bmkg.go.id>
2025-04-20 14:41:40 +0700

    Text_formatter: add tutorial on how to use it

--- text_formatter/urls.py
+++ text_formatter/urls.py
@@ -1,10 +1,12 @@
 from django.views import View
 from django.http import HttpResponse
 from django.urls import path
-from .views import TextFormatterView
+from .views import TextFormatterView, TutorialView
 from . import views
 
 app_name = 'text_formatter'
 
 urlpatterns = [
-    path('', TextFormatterView.as_view(), name='text_formatter_view'),]
+    path('', TextFormatterView.as_view(), name='text_formatter_view'),
+    path('tutorial/', TutorialView.as_view(), name='text_formatter_tutorial'),
+]
